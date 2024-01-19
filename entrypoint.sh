ac379c0ce03000dd91939d2d5f0d2ef8fc1d3814
Chris Simmons <chris.dev@netchris.com>
2024-01-18 19:44:41 -0800

    WIP
    [skip ci]

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,50 @@
 #!/bin/sh -l
 
-Version="$1"
-
-PrefixStrippingRegEx="^v?(.*)$"
-
-Version=$(echo $Version | pcre2grep -o1 $PrefixStrippingRegEx)
+GitRef="$1"
 
 # https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
-SemVer2RegEx="^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
+# This is relaxed to find it the first time in a line
+SemVer2RegEx="((0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)"
 
-semVer=$(echo $Version | pcre2grep $SemVer2RegEx)
-semVerMajor=$(echo $Version | pcre2grep -o1 $SemVer2RegEx)
-semVerMinor=$(echo $Version | pcre2grep -o2 $SemVer2RegEx)
-semVerPatch=$(echo $Version | pcre2grep -o3 $SemVer2RegEx)
-semVerPreRelease=$(echo $Version | pcre2grep -o4 $SemVer2RegEx)
-semVerBuildMetadata=$(echo $Version | pcre2grep -o5 $SemVer2RegEx)
+Version=$(echo $GitRef | pcre2grep $SemVer2RegEx)
 
-MajorMinorRegEx="^((0|[1-9]\d*)\.(0|[1-9]\d*))"
+semVer=$(echo $Version | pcre2grep -o1 $SemVer2RegEx)
+semVerMajor=$(echo $Version | pcre2grep -o2 $SemVer2RegEx)
+semVerMinor=$(echo $Version | pcre2grep -o3 $SemVer2RegEx)
+semVerPatch=$(echo $Version | pcre2grep -o4 $SemVer2RegEx)
+semVerPreRelease=$(echo $Version | pcre2grep -o5 $SemVer2RegEx)
+semVerBuildMetadata=$(echo $Version | pcre2grep -o6 $SemVer2RegEx)
+
+MajorMinorRegEx="((0|[1-9]\d*)\.(0|[1-9]\d*))"
 majorMinorOnly=$(echo $Version | pcre2grep -o1 $MajorMinorRegEx)
 
-echo "semVer=$semVer" >> $GITHUB_OUTPUT
-echo "semVerMajor=$semVerMajor" >> $GITHUB_OUTPUT
-echo "semVerMinor=$semVerMinor" >> $GITHUB_OUTPUT
-echo "semVerPatch=$semVerPatch" >> $GITHUB_OUTPUT
-echo "semVerPreRelease=$semVerPreRelease" >> $GITHUB_OUTPUT
-echo "semVerBuildMetadata=$semVerBuildMetadata" >> $GITHUB_OUTPUT
-echo "majorMinorOnly=$majorMinorOnly" >> $GITHUB_OUTPUT
+hasSemVer=false
+isPreRelease=false
+
+if [ ! -z "$semVer" ]
+then
+  hasSemVer=true
+
+  if [ ! -z "$semVerPreRelease" ]
+  then
+    isPreRelease=true
+  fi
+fi
+
+# echo "semVer=$semVer" >> $GITHUB_OUTPUT
+# echo "semVerMajor=$semVerMajor" >> $GITHUB_OUTPUT
+# echo "semVerMinor=$semVerMinor" >> $GITHUB_OUTPUT
+# echo "semVerPatch=$semVerPatch" >> $GITHUB_OUTPUT
+# echo "semVerPreRelease=$semVerPreRelease" >> $GITHUB_OUTPUT
+# echo "semVerBuildMetadata=$semVerBuildMetadata" >> $GITHUB_OUTPUT
+# echo "majorMinorOnly=$majorMinorOnly" >> $GITHUB_OUTPUT
+
+echo "hasSemVer=$hasSemVer"
+echo "semVer=$semVer"
+echo "semVerMajor=$semVerMajor"
+echo "semVerMinor=$semVerMinor"
+echo "semVerPatch=$semVerPatch"
+echo "semVerPreRelease=$semVerPreRelease"
+echo "semVerBuildMetadata=$semVerBuildMetadata"
+echo "majorMinorOnly=$majorMinorOnly"
+echo "isPreRelease=$isPreRelease"
